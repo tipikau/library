54e8f21751294f7164cfc37415d97d138346e1d9
Человек <nik21.dance@gmail.com>
2023-11-30 06:25:24 +0700

    updated my icons and Book_reader model

--- views/site/login.php
+++ views/site/login.php
@@ -1,14 +1,24 @@
-<h2>Авторизация</h2>
-<h3><?= $message ?? ''; ?></h3>
-
-<h3><?= app()->auth->user()->name ?? ''; ?></h3>
-<?php
-if (!app()->auth::check()):
+<div class="form-container">
+    <p class="logo"><img class="logo-img" src="../../public/static/media/lib_logo.png">Библиотека</p>
+    <h2>Вход в систему</h2>
+    <h3><?= $message ?? ''; ?></h3>
+    <h3><?= app()->auth->user()->name ?? ''; ?></h3>
+    <?php
+    if (!app()->auth::check()):
     ?>
     <form method="post">
         <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
-        <button>Войти</button>
+        <label>Логин<input type="text" name="login"></label>
+        <label>Пароль<input type="password" name="password"></label>
+        <button class="submit-btn">Войти</button>
     </form>
+</div>
 <?php endif;
+
+?>
+
+
+
+
+
+
